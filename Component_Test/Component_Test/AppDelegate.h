fcab2db46849c7cbf08ce223a5c0f98db1f17b7d
哈哈哥 <shenjifeng777@icloud.com>
2020-04-05 01:56:32 +0800

    1

---
+++ Component_Test/Component_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Component_Test
+//
+//  Created by Apple on 2020/4/5.
+//  Copyright © 2020 com.fengge.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
